5360b2d7d866a5ea7cb4118efae0089c4cc5d0d4
Chokobo <Chokobo@Chokobo.com>
2021-11-18 00:29:00 -0500

    initial push

---
+++ OSCP Practice Exam Generator/Form1.Designer.cs
@@ -0,0 +1,236 @@
+﻿
+namespace OSCP_Practice_Exam_Generator
+{
+    partial class form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(form1));
+            this.btnGenerate = new System.Windows.Forms.Button();
+            this.rtbOut = new System.Windows.Forms.RichTextBox();
+            this.cbVuln = new System.Windows.Forms.CheckBox();
+            this.cbPG = new System.Windows.Forms.CheckBox();
+            this.cbHTB = new System.Windows.Forms.CheckBox();
+            this.cbHarder = new System.Windows.Forms.CheckBox();
+            this.btnStart = new System.Windows.Forms.Button();
+            this.btnStop = new System.Windows.Forms.Button();
+            this.groupBox1 = new System.Windows.Forms.GroupBox();
+            this.lblStopWatch = new System.Windows.Forms.Label();
+            this.btnReset = new System.Windows.Forms.Button();
+            this.lblTimer = new System.Windows.Forms.Timer(this.components);
+            this.richTextBox1 = new System.Windows.Forms.RichTextBox();
+            this.statusStrip1 = new System.Windows.Forms.StatusStrip();
+            this.groupBox1.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // btnGenerate
+            // 
+            this.btnGenerate.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnGenerate.Location = new System.Drawing.Point(12, 258);
+            this.btnGenerate.Name = "btnGenerate";
+            this.btnGenerate.Size = new System.Drawing.Size(295, 39);
+            this.btnGenerate.TabIndex = 0;
+            this.btnGenerate.Text = "Create Practice Exam";
+            this.btnGenerate.UseVisualStyleBackColor = true;
+            this.btnGenerate.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // rtbOut
+            // 
+            this.rtbOut.Location = new System.Drawing.Point(12, 39);
+            this.rtbOut.Name = "rtbOut";
+            this.rtbOut.Size = new System.Drawing.Size(295, 213);
+            this.rtbOut.TabIndex = 1;
+            this.rtbOut.Text = "";
+            // 
+            // cbVuln
+            // 
+            this.cbVuln.AutoSize = true;
+            this.cbVuln.Location = new System.Drawing.Point(12, 16);
+            this.cbVuln.Name = "cbVuln";
+            this.cbVuln.Size = new System.Drawing.Size(67, 17);
+            this.cbVuln.TabIndex = 2;
+            this.cbVuln.Text = "VulnHub";
+            this.cbVuln.UseVisualStyleBackColor = true;
+            // 
+            // cbPG
+            // 
+            this.cbPG.AutoSize = true;
+            this.cbPG.Checked = true;
+            this.cbPG.CheckState = System.Windows.Forms.CheckState.Checked;
+            this.cbPG.Location = new System.Drawing.Point(85, 16);
+            this.cbPG.Name = "cbPG";
+            this.cbPG.Size = new System.Drawing.Size(105, 17);
+            this.cbPG.TabIndex = 3;
+            this.cbPG.Text = "Proving Grounds";
+            this.cbPG.UseVisualStyleBackColor = true;
+            // 
+            // cbHTB
+            // 
+            this.cbHTB.AutoSize = true;
+            this.cbHTB.Checked = true;
+            this.cbHTB.CheckState = System.Windows.Forms.CheckState.Checked;
+            this.cbHTB.Location = new System.Drawing.Point(196, 16);
+            this.cbHTB.Name = "cbHTB";
+            this.cbHTB.Size = new System.Drawing.Size(91, 17);
+            this.cbHTB.TabIndex = 4;
+            this.cbHTB.Text = "Hack the Box";
+            this.cbHTB.UseVisualStyleBackColor = true;
+            // 
+            // cbHarder
+            // 
+            this.cbHarder.AutoSize = true;
+            this.cbHarder.Location = new System.Drawing.Point(463, 12);
+            this.cbHarder.Name = "cbHarder";
+            this.cbHarder.Size = new System.Drawing.Size(131, 17);
+            this.cbHarder.TabIndex = 5;
+            this.cbHarder.Text = "Include harder boxes?";
+            this.cbHarder.UseVisualStyleBackColor = true;
+            // 
+            // btnStart
+            // 
+            this.btnStart.Location = new System.Drawing.Point(17, 19);
+            this.btnStart.Name = "btnStart";
+            this.btnStart.Size = new System.Drawing.Size(75, 23);
+            this.btnStart.TabIndex = 6;
+            this.btnStart.Text = "Start";
+            this.btnStart.UseVisualStyleBackColor = true;
+            this.btnStart.Click += new System.EventHandler(this.btnStart_Click);
+            // 
+            // btnStop
+            // 
+            this.btnStop.Location = new System.Drawing.Point(98, 19);
+            this.btnStop.Name = "btnStop";
+            this.btnStop.Size = new System.Drawing.Size(75, 23);
+            this.btnStop.TabIndex = 7;
+            this.btnStop.Text = "Stop";
+            this.btnStop.UseVisualStyleBackColor = true;
+            this.btnStop.Click += new System.EventHandler(this.btnStop_Click);
+            // 
+            // groupBox1
+            // 
+            this.groupBox1.Anchor = ((System.Windows.Forms.AnchorStyles)(((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.groupBox1.Controls.Add(this.lblStopWatch);
+            this.groupBox1.Controls.Add(this.btnReset);
+            this.groupBox1.Controls.Add(this.btnStart);
+            this.groupBox1.Controls.Add(this.btnStop);
+            this.groupBox1.Location = new System.Drawing.Point(313, 39);
+            this.groupBox1.Name = "groupBox1";
+            this.groupBox1.Size = new System.Drawing.Size(281, 128);
+            this.groupBox1.TabIndex = 8;
+            this.groupBox1.TabStop = false;
+            this.groupBox1.Text = "Timer";
+            // 
+            // lblStopWatch
+            // 
+            this.lblStopWatch.AutoSize = true;
+            this.lblStopWatch.Font = new System.Drawing.Font("Microsoft Sans Serif", 14F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lblStopWatch.Location = new System.Drawing.Point(93, 76);
+            this.lblStopWatch.Name = "lblStopWatch";
+            this.lblStopWatch.Size = new System.Drawing.Size(80, 24);
+            this.lblStopWatch.TabIndex = 9;
+            this.lblStopWatch.Text = "00:00:00";
+            // 
+            // btnReset
+            // 
+            this.btnReset.Location = new System.Drawing.Point(179, 19);
+            this.btnReset.Name = "btnReset";
+            this.btnReset.Size = new System.Drawing.Size(75, 23);
+            this.btnReset.TabIndex = 8;
+            this.btnReset.Text = "Reset";
+            this.btnReset.UseVisualStyleBackColor = true;
+            this.btnReset.Click += new System.EventHandler(this.btnReset_Click);
+            // 
+            // lblTimer
+            // 
+            this.lblTimer.Enabled = true;
+            this.lblTimer.Tick += new System.EventHandler(this.lblTimer_Tick);
+            // 
+            // richTextBox1
+            // 
+            this.richTextBox1.Anchor = ((System.Windows.Forms.AnchorStyles)((((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Bottom) 
+            | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.richTextBox1.Location = new System.Drawing.Point(313, 173);
+            this.richTextBox1.Name = "richTextBox1";
+            this.richTextBox1.ReadOnly = true;
+            this.richTextBox1.Size = new System.Drawing.Size(280, 124);
+            this.richTextBox1.TabIndex = 9;
+            this.richTextBox1.Text = resources.GetString("richTextBox1.Text");
+            // 
+            // statusStrip1
+            // 
+            this.statusStrip1.Location = new System.Drawing.Point(0, 316);
+            this.statusStrip1.Name = "statusStrip1";
+            this.statusStrip1.Size = new System.Drawing.Size(606, 22);
+            this.statusStrip1.TabIndex = 10;
+            this.statusStrip1.Text = "statusStrip1";
+            // 
+            // form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(606, 338);
+            this.Controls.Add(this.statusStrip1);
+            this.Controls.Add(this.richTextBox1);
+            this.Controls.Add(this.groupBox1);
+            this.Controls.Add(this.cbHarder);
+            this.Controls.Add(this.cbHTB);
+            this.Controls.Add(this.cbPG);
+            this.Controls.Add(this.cbVuln);
+            this.Controls.Add(this.rtbOut);
+            this.Controls.Add(this.btnGenerate);
+            this.Name = "form1";
+            this.Text = "Practice Exam Generator";
+            this.Load += new System.EventHandler(this.form1_Load);
+            this.groupBox1.ResumeLayout(false);
+            this.groupBox1.PerformLayout();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button btnGenerate;
+        private System.Windows.Forms.RichTextBox rtbOut;
+        private System.Windows.Forms.CheckBox cbVuln;
+        private System.Windows.Forms.CheckBox cbPG;
+        private System.Windows.Forms.CheckBox cbHTB;
+        private System.Windows.Forms.CheckBox cbHarder;
+        private System.Windows.Forms.Button btnStart;
+        private System.Windows.Forms.Button btnStop;
+        private System.Windows.Forms.GroupBox groupBox1;
+        private System.Windows.Forms.Label lblStopWatch;
+        private System.Windows.Forms.Button btnReset;
+        private System.Windows.Forms.Timer lblTimer;
+        private System.Windows.Forms.RichTextBox richTextBox1;
+        private System.Windows.Forms.StatusStrip statusStrip1;
+    }
+}
+
---
+++ OSCP Practice Exam Generator/Form1.cs
@@ -0,0 +1,300 @@
+﻿using System;
+using System.Diagnostics;
+using System.Timers;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace OSCP_Practice_Exam_Generator
+{
+    public partial class form1 : Form
+    {
+        static Stopwatch sw = new Stopwatch();
+        static System.Timers.Timer t = new System.Timers.Timer(1000);
+
+        private void form1_Load(object sender, EventArgs e)
+        {
+            
+        }
+
+        private void lblTimer_Tick(object sender, EventArgs e)
+        {
+            updateTimer();
+        }
+
+        private void updateTimer()
+        {
+            TimeSpan ts = sw.Elapsed;
+            int  eHours = ts.Hours;
+            int eMinutes = ts.Minutes;
+            int eSeconds = ts.Seconds;
+
+            string tstring = "";
+
+            if (eHours < 10) { tstring = tstring + "0" + eHours + ":"; } else { tstring = tstring + eHours + ":"; }
+            if (eMinutes < 10) { tstring = tstring + "0" + eMinutes + ":"; } else { tstring = tstring + eMinutes + ":"; }
+            if (eSeconds < 10) { tstring = tstring + "0" + eSeconds; } else { tstring = tstring + eSeconds; }
+
+
+
+            //lblStopWatch.Text = "Hello World";
+            lblStopWatch.Text = tstring;
+        }
+
+        public form1()
+        {
+            InitializeComponent();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+
+            sw.Restart();
+            sw.Start();
+
+
+            rtbOut.Text = "";
+            List<string> winboxes = new List<string>();
+            List<string> linboxes = new List<string>();
+
+            //Include machines which were deemed harder than OSCP
+            if (cbHarder.Checked) 
+            {
+                if (cbHTB.Checked)
+                {
+                    winboxes.Add("Jeeves - HTB");
+                    winboxes.Add("Bart - HTB");
+                    winboxes.Add("Tally - HTB");
+                    winboxes.Add("Netmon - HTB");
+                    winboxes.Add("Sizzle - HTB");
+                    winboxes.Add("Sniper - HTB");
+                    winboxes.Add("Control - HTB");
+                    winboxes.Add("Nest - HTB");
+                    winboxes.Add("Sauna - HTB");
+                    winboxes.Add("Cascade - HTB");
+                    winboxes.Add("Querier - HTB");
+                    winboxes.Add("BlackField - HTB");
+                    winboxes.Add("APT - HTB");
+                    winboxes.Add("Atom - HTB");
+
+                    linboxes.Add("Kotarak - HTB");
+                    linboxes.Add("Falafel - HTB");
+                    linboxes.Add("Devops - HTB");
+                    linboxes.Add("Hawk - HTB");
+                    linboxes.Add("Lightweight - HTB");
+                    linboxes.Add("La Casa Del Papel - HTB");
+                    linboxes.Add("Jail - HTB");
+                    linboxes.Add("Safe - HTB");
+                    linboxes.Add("Bitlab - HTB");
+                    linboxes.Add("October - HTB");
+                    linboxes.Add("Mango - HTB");
+                    linboxes.Add("Book - HTB");
+                    linboxes.Add("Quick - HTB");
+                }
+
+                if (cbPG.Checked)
+                {
+                    linboxes.Add("Bratarina - PG Practice");
+                    linboxes.Add("Clyde - PG Practice");
+                    linboxes.Add("Shifty - PG Practice");
+                    linboxes.Add("XposedAPI - PG Practice");
+                    linboxes.Add("Twiggy - PG Practice");
+
+                    winboxes.Add("Internal - PG Practice");
+                    winboxes.Add("Vector - PG Practice");
+                    winboxes.Add("HelpDesk - PG Practice");
+                }
+            }
+
+            //Include PG Practice Machines
+            if (cbPG.Checked)
+            {
+                winboxes.Add("Nickel - PG Practice");
+                winboxes.Add("Slort - PG Practice");
+                winboxes.Add("Authby - PG Practice");
+                winboxes.Add("Jacko - PG Practice");
+                winboxes.Add("Meathead - PG Practice");
+                winboxes.Add("UT99 - PG Practice");
+                winboxes.Add("MedJed - PG Practice");
+                winboxes.Add("Algeron - PG Practice");
+                winboxes.Add("BillyBoss - PG Practice");
+                winboxes.Add("Kevin - PG Practice");
+                winboxes.Add("Butch - PG Practice");
+                winboxes.Add("Metallus - PG Practice");
+
+                linboxes.Add("ClamAV - PG Practice");
+                linboxes.Add("Wombo - PG Practice");
+                linboxes.Add("Payday - PG Practice");
+                linboxes.Add("Fail - PG Practice");
+                linboxes.Add("Nibbles - PG Practice");
+                linboxes.Add("Banzai - PG Practice");
+                linboxes.Add("Hunit - PG Practice");
+                linboxes.Add("Dibble - PG Practice");
+                linboxes.Add("Zino - PG Practice");
+                linboxes.Add("Hetemit - PG Practice");
+                linboxes.Add("Peppo - PG Practice");
+                linboxes.Add("Postfish - PG Practice");
+                //linboxes.Add("Malbec - PG Practice"); Removed not simulating BOF
+                linboxes.Add("Sybaris - PG Practice");
+            }
+
+            //Include Hack The Box machines
+            if (cbHTB.Checked)
+            {
+                winboxes.Add("Legacy - HTB");
+                winboxes.Add("Blue - HTB");
+                winboxes.Add("Devel - HTB");
+                winboxes.Add("Optimum - HTB");
+                winboxes.Add("Bastard - HTB");
+                winboxes.Add("Granny - HTB");
+                winboxes.Add("Arctic - HTB");
+                winboxes.Add("Grandpa - HTB");
+                winboxes.Add("Silo - HTB");
+                winboxes.Add("Bounty - HTB");
+                winboxes.Add("Jerry - HTB");
+                winboxes.Add("Conceal - HTB");
+                winboxes.Add("Chatterbox - HTB");
+                winboxes.Add("Forest - HTB");
+                winboxes.Add("BankRobber - HTB");
+                winboxes.Add("Secnotes - HTB");
+                winboxes.Add("Bastion - HTB");
+                winboxes.Add("Buff - HTB");
+                winboxes.Add("Servmon - HTB");
+                winboxes.Add("Active - HTB");
+                winboxes.Add("Remote - HTB");
+                winboxes.Add("Fuse - HTB");
+                winboxes.Add("Omni - HTB");
+                winboxes.Add("Worker - HTB");
+
+                linboxes.Add("Lame - HTB");
+                linboxes.Add("Brainfuck - HTB");
+                linboxes.Add("Shocker - HTB");
+                linboxes.Add("Bashed - HTB");
+                linboxes.Add("Nibbles - HTB");
+                linboxes.Add("Beep - HTB");
+                linboxes.Add("Cronos - HTB");
+                linboxes.Add("Nineveh - HTB");
+                linboxes.Add("Sense - HTB");
+                linboxes.Add("Solidstate - HTB");
+                linboxes.Add("Node - HTB");
+                linboxes.Add("Valentine - HTB");
+                linboxes.Add("Poison - HTB");
+                linboxes.Add("Sunday - HTB");
+                linboxes.Add("Tartarsauce - HTB");
+                linboxes.Add("Irked - HTB");
+                linboxes.Add("Friendzone - HTB");
+                linboxes.Add("Swagshop - HTB");
+                linboxes.Add("Networked - HTB");
+                linboxes.Add("Jarvis - HTB");
+                linboxes.Add("Mirai - HTB");
+                linboxes.Add("Popcorn - HTB");
+                linboxes.Add("Haircut - HTB");
+                linboxes.Add("Blocky - HTB");
+                linboxes.Add("Frolic - HTB");
+                linboxes.Add("Postman - HTB");
+                linboxes.Add("Traverxec - HTB");
+                linboxes.Add("OpenAdmin - HTB");
+                linboxes.Add("Magic - HTB");
+                linboxes.Add("Admirer - HTB");
+                linboxes.Add("Blunder - HTB");
+                linboxes.Add("Tabby - HTB");
+                linboxes.Add("Doctor - HTB");
+                linboxes.Add("SneakyMailer - HTB");
+                linboxes.Add("Passage - HTB");
+                linboxes.Add("Luanne - HTB");
+                linboxes.Add("Time - HTB");
+                linboxes.Add("Ready - HTB");
+                linboxes.Add("Delivery - HTB");
+                linboxes.Add("Ophiuchi - HTB");
+            }
+
+
+            if (cbVuln.Checked)
+            {
+                linboxes.Add("DC 9: https://www.vulnhub.com/entry/dc-9,412/");
+                linboxes.Add("Digitalworld.local (Bravery): https://www.vulnhub.com/entry/digitalworldlocal-bravery,281/");
+                linboxes.Add("Digitalworld.local (Development): https://www.vulnhub.com/entry/digitalworldlocal-development,280/");
+                linboxes.Add("Digitalworld.local (Mercy v2): https://www.vulnhub.com/entry/digitalworldlocal-mercy-v2,263/");
+                linboxes.Add("Digitalworld.local (JOY): https://www.vulnhub.com/entry/digitalworldlocal-joy,298/");
+                linboxes.Add("Prime 1: https://www.vulnhub.com/entry/prime-1,358/");
+                linboxes.Add("Symfonos 1: https://www.vulnhub.com/entry/symfonos-1,322/");
+                linboxes.Add("Symfonos 2: https://www.vulnhub.com/entry/symfonos-2,331/");
+                linboxes.Add("Symfonos 3: https://www.vulnhub.com/entry/symfonos-3,332/");
+                linboxes.Add("Symfonos 4: https://www.vulnhub.com/entry/symfonos-4,347/");
+                linboxes.Add("Symfonos 5.2: https://www.vulnhub.com/entry/symfonos-52,415/");
+                linboxes.Add("Misdirection 1: https://www.vulnhub.com/entry/misdirection-1,371/");
+                linboxes.Add("Sar 1: https://www.vulnhub.com/entry/sar-1,425/");
+                linboxes.Add("Djinn 1: https://www.vulnhub.com/entry/djinn-1,397/");
+                linboxes.Add("EVM 1: https://www.vulnhub.com/entry/evm-1,391/");
+                linboxes.Add("DerpNStink 1: https://www.vulnhub.com/entry/derpnstink-1,221/");
+                linboxes.Add("RickdiculouslyEasy 1: https://www.vulnhub.com/entry/rickdiculouslyeasy-1,207/");
+                linboxes.Add("Tommy Boy 1: https://www.vulnhub.com/entry/tommy-boy-1,157/");
+                linboxes.Add("Breach 1: https://www.vulnhub.com/entry/breach-1,152/");
+                linboxes.Add("Breach 2.1: https://www.vulnhub.com/entry/breach-21,159/");
+                linboxes.Add("Breach 3.0.1: https://www.vulnhub.com/entry/breach-301,177/");
+                linboxes.Add("NullByte: https://www.vulnhub.com/entry/nullbyte-1,126/");
+                linboxes.Add("Bob 1.0.1: https://www.vulnhub.com/entry/bob-101,226/");
+                linboxes.Add("Toppo 1: https://www.vulnhub.com/entry/toppo-1,245/");
+                linboxes.Add("W34kn3ss 1: https://www.vulnhub.com/entry/w34kn3ss-1,270/");
+                linboxes.Add("GoldenEye 1: https://www.vulnhub.com/entry/goldeneye-1,240/");
+                linboxes.Add("Infosec Prep OSCP Box: https://www.vulnhub.com/entry/infosec-prep-oscp,508/");
+                linboxes.Add("LemonSqueezy: https://www.vulnhub.com/entry/lemonsqueezy-1,473/");
+                linboxes.Add("Brainpan 1: https://www.vulnhub.com/entry/brainpan-1,51/");
+                linboxes.Add("Pinkys Palace v1: https://www.vulnhub.com/entry/pinkys-palace-v1,225/");
+                linboxes.Add("Lord of the root 1.0.1: https://www.vulnhub.com/entry/lord-of-the-root-101,129/");
+                linboxes.Add("Tiki-1: https://www.vulnhub.com/entry/tiki-1,525/");
+                linboxes.Add("Healthcare 1: https://www.vulnhub.com/entry/healthcare-1,522/");
+                linboxes.Add("Photographer 1: https://www.vulnhub.com/entry/photographer-1,519/");
+                linboxes.Add("Glasglow 1.1: https://www.vulnhub.com/entry/glasgow-smile-11,491/");
+                linboxes.Add("DevGuru 1: https://www.vulnhub.com/entry/devguru-1,620/");
+                linboxes.Add("Alpha 1: https://www.vulnhub.com/entry/alfa-1,655/");
+            }
+
+
+            Random r = new Random();
+
+            rtbOut.AppendText("-- LINUX --\n");
+            for (int i = 1; i <= 2; i++)
+            {
+                int id = r.Next(0, (linboxes.Count - 1));
+                rtbOut.AppendText(linboxes[id] + "\n");
+                linboxes.RemoveAt(id);
+            }
+
+            rtbOut.AppendText("\n\n");
+            rtbOut.AppendText("-- WIN --\n");
+
+            for (int i = 1; i <= 2; i++)
+            {
+                int id = r.Next(0, (winboxes.Count - 1));
+                rtbOut.AppendText(winboxes[id] + "\n");
+                winboxes.RemoveAt(id);
+            }
+
+
+        }
+
+
+        private void btnStop_Click(object sender, EventArgs e)
+        {
+            sw.Stop();
+        }
+
+        private void btnReset_Click(object sender, EventArgs e)
+        {
+            sw.Restart();
+        }
+
+        private void btnStart_Click(object sender, EventArgs e)
+        {
+            sw.Start();
+        }
+
+        
+    }
+}
